8b08866b767b89cd6d44e66df5e4d8739ffe29fb
Elsayed Hassan <eh600039@gmail.com>
2023-05-08 14:30:56 +0300

    finish the project

---
+++ dist/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.owl-one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+  })
+  $('.owl-two').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+  })
+});
